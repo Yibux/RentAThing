9e183ce80ca14f129de0c2b97e1992466ba21aa2
asitarcz <01168957@pw.edu.pl>
2026-01-04 22:37:29 +0100

    Wyjątki firebase i działające logowanie

--- src/RentalSystem.Client.Web/wwwroot/firebaseAuth.js
+++ src/RentalSystem.Client.Web/wwwroot/firebaseAuth.js
@@ -1,3 +1,36 @@
+//window.firebaseAuth = {
+//    register: async function (email, password) {
+//        if (!firebase.apps.length) {
+//            firebase.initializeApp(window.firebaseConfig);
+//        }
+
+//        try {
+//            const userCredential = await firebase.auth().createUserWithEmailAndPassword(email, password);
+//            return await userCredential.user.getIdToken();
+//        } catch (err) {
+//            const code = err.code || "unknown_error";
+//            throw code;
+//            //throw new Error(code);
+//        }
+//    },
+
+//    login: async function (email, password) {
+//        if (!firebase.apps.length) {
+//            firebase.initializeApp(window.firebaseConfig);
+//        }
+
+//        try {
+//            const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password);
+//            return await userCredential.user.getIdToken();
+//        } catch (err) {
+//            const code = err.code || "unknown_error";
+//            throw code;
+//            //throw new Error(code);
+//        }
+//    }
+//};
+
+
 window.firebaseAuth = {
     register: async function (email, password) {
         if (!firebase.apps.length) {
@@ -6,10 +39,24 @@ window.firebaseAuth = {
 
         try {
             const userCredential = await firebase.auth().createUserWithEmailAndPassword(email, password);
-            return await userCredential.user.getIdToken();
+            const token = await userCredential.user.getIdToken();
+            return { success: true, token: token, error: null }; // Zwracamy obiekt
         } catch (err) {
-            const code = err.code || "unknown_error";
-            throw code;
+            return { success: false, token: null, error: err.code || "unknown_error" }; // Nie rzucamy b³êdu!
+        }
+    },
+
+    login: async function (email, password) {
+        if (!firebase.apps.length) {
+            firebase.initializeApp(window.firebaseConfig);
+        }
+
+        try {
+            const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password);
+            const token = await userCredential.user.getIdToken();
+            return { success: true, token: token, error: null };
+        } catch (err) {
+            return { success: false, token: null, error: err.code || "unknown_error" };
         }
     }
 };
